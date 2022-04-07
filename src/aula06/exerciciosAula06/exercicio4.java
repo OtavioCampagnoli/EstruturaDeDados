9e38290a1a108df816cad7329da3665dcf46ce1c
Otávio Campagnoli <86927698+OtavioCampagnoli@users.noreply.github.com>
2022-04-07 13:07:09 -0300

    060422

---
+++ src/aula06/exerciciosAula06/exercicio4.java
@@ -0,0 +1,13 @@
+package aula06.exerciciosAula06;
+
+public class exercicio4 {
+    // declarando atributos
+    int i, j;
+    String vetA[];
+    String vetB[];
+    String result[];
+
+    
+
+
+}
